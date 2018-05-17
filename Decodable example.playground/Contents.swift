2630119412628647bf50c3f49e12883730b41ecb
Pradeep Rajkumar <rajkumar.pradeep@yahoo.com>
2018-05-17 13:49:40 +0530

    Update Contents.swift

--- Decodable example.playground/Contents.swift
+++ Decodable example.playground/Contents.swift
@@ -6,11 +6,11 @@ let data: Data
 struct Organisation {
     struct Employee: Decodable {
         var designation: String
-        var scheda_tecnica: [String:String]?
-        var descrizione: String
+        var schedaTecnica: [String:String]?
+        var description: String
         var iscrizione: String
         var materiali: String
-        var notizie_storiche: String?
+        var notizieStoriche: String?
         var image: String?
     }
     var employees: [Employee]
@@ -30,11 +30,11 @@ extension Organisation: Decodable {
         init?(intValue: Int) {
             return nil
         }
-        static let descrizione = EmployeeKey(stringValue: "descrizione")
-        static let scheda_tecnica = EmployeeKey(stringValue: "scheda_tecnica")
+        static let description = EmployeeKey(stringValue: "descrizione")
+        static let schedaTecnica = EmployeeKey(stringValue: "scheda_tecnica")
         static let iscrizione = EmployeeKey(stringValue: "iscrizione")
         static let materiali = EmployeeKey(stringValue: "materiali")
-        static let notizie_storiche = EmployeeKey(stringValue: "notizie_storiche")
+        static let notizieStoriche = EmployeeKey(stringValue: "notizie_storiche")
         static let image = EmployeeKey(stringValue: "image")
     }
     
@@ -45,14 +45,14 @@ extension Organisation: Decodable {
             
             let employeeContainer = try container.nestedContainer(keyedBy: EmployeeKey.self, forKey: key)
             
-            let descrizione = try employeeContainer.decode(String.self, forKey: EmployeeKey.descrizione!)
-            let scheda_tecnica = try employeeContainer.decode([String: String].self, forKey: EmployeeKey.scheda_tecnica!)
+            let description = try employeeContainer.decode(String.self, forKey: EmployeeKey.description!)
+            let schedaTecnica = try employeeContainer.decode([String: String].self, forKey: EmployeeKey.schedaTecnica!)
             let iscrizione = try employeeContainer.decode(String.self, forKey: EmployeeKey.iscrizione!)
             let materiali = try employeeContainer.decode(String.self, forKey: EmployeeKey.materiali!)
-            let notizie_storiche = try employeeContainer.decode(String.self, forKey: EmployeeKey.notizie_storiche!)
-            let image = try employeeContainer.decode(String.self, forKey: EmployeeKey.image!)
+            let notizieStoriche = try employeeContainer.decode(String.self, forKey: EmployeeKey.notizieStoriche!)
+            let image = try employeeContainer.decodeIfPresent(String.self, forKey: EmployeeKey.image!)
             
-            let employee = Employee(designation: key.stringValue, scheda_tecnica: scheda_tecnica, descrizione: descrizione, iscrizione: iscrizione, materiali: materiali, notizie_storiche: notizie_storiche, image: image)
+            let employee = Employee(designation: key.stringValue, schedaTecnica: schedaTecnica, description: description, iscrizione: iscrizione, materiali: materiali, notizieStoriche: notizieStoriche, image: image)
             employees.append(employee)
         }
         self.init(employees: employees)
@@ -68,7 +68,8 @@ do {
     let decoder = JSONDecoder()
     if let decodedOrganisation = try? decoder.decode(Organisation.self, from: data) {
         for employee in decodedOrganisation.employees {
-            print(employee.designation, employee.descrizione, employee.iscrizione, employee.scheda_tecnica, employee.image, employee.notizie_storiche)
+            print(employee.designation)
+            print(employee.description, employee.iscrizione, employee.schedaTecnica, employee.image, employee.notizieStoriche)
             
         }
     }
